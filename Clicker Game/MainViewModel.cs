4ecfa3f1d3bcbaf828c6821d3638b56025f29fbf
Guli_tp48@itstep.edu.az <Guli_tp48@itstep.edu.az>
2022-07-12 20:41:41 +0400

    Add project files.

---
+++ Clicker Game/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+
+namespace Clicker_Game
+{
+    [INotifyPropertyChanged]
+    public partial class MainViewModel
+    {
+        DateTime Begin;
+        readonly Random random = new();
+        [ObservableProperty]
+        double _dif;
+        [ObservableProperty]
+        Brush _color = Brushes.Aqua;
+        [ObservableProperty] 
+        bool _enabled = false;
+        [ICommand]
+        void Start()
+        {
+            Thread Change = new Thread(() =>
+            {
+                Color = Brushes.Red;
+                Thread.Sleep(random.Next(2000, 7000));
+                Enabled = true;
+                Color = Brushes.Green;
+                Begin = DateTime.Now;
+            })
+            {
+                IsBackground = true
+            };
+            Change.Start();
+        }
+        [ICommand]
+        void ClickMe()
+        {
+            Dif = (DateTime.Now - Begin).TotalSeconds;
+            Color = Brushes.Aqua;
+            Enabled = false;
+        }
+    }
+}
